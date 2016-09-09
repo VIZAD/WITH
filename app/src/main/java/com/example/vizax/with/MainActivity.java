d1eb7e3d8da9cc9c8cc74001db9e4de601bb5e47
leo0629 <13986347@qq.com>
2016-09-09 16:47:02 +0800

    code change

--- app/src/main/java/com/example/vizax/with/MainActivity.java
+++ app/src/main/java/com/example/vizax/with/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("肥鹏code");
+        System.out.println("肥鹏code.ok");
     }
 }

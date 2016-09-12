af0d1d6698ce576dbec8aac50dbc7e914758d261
prj <zquprj@gmail.com>
2016-09-13 01:11:33 +0800

    一些base封装、util工具类                                                        --PRJ

--- app/src/main/java/com/example/vizax/with/ui/MainActivity.java
+++ app/src/main/java/com/example/vizax/with/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.vizax.with;
+package com.example.vizax.with.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.vizax.with.R;
 
 public class MainActivity extends AppCompatActivity {
 

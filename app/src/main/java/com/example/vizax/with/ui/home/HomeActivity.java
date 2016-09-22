c0d8aaeec227ca7bd93c682ad39834ee46b34cd0
Zxwwxz <274693193@qq.com>
2016-09-22 10:44:33 +0800

    主界面跳转

--- app/src/main/java/com/example/vizax/with/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/vizax/with/ui/home/HomeActivity.java
@@ -3,12 +3,13 @@ package com.example.vizax.with.ui.home;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -17,14 +18,17 @@ import com.example.vizax.with.base.BaseActivity;
 import com.example.vizax.with.bean.InvitationBean;
 import com.example.vizax.with.bean.MembersBean;
 import com.example.vizax.with.customView.BaseToolBar;
+import com.example.vizax.with.ui.Insist.InsistActivity;
 import com.example.vizax.with.ui.invitationList.InvitationActivity;
 import com.example.vizax.with.ui.invitationList.InvitationDetailsActivity;
+import com.example.vizax.with.ui.myconcern.MyConcernActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeActivity extends BaseActivity implements HomeContact.View {
 
@@ -40,7 +44,26 @@ public class HomeActivity extends BaseActivity implements HomeContact.View {
     @BindView(R.id.sidebar_menu)
     LinearLayout mSideBar;
     @BindView(R.id.drawerlayout_id)
-    MyDrawerLayout mDrawerLayout;
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.my_info_lLayout)
+    LinearLayout myInfoLLayout;
+    @BindView(R.id.my_invitation_txtVi)
+    TextView myInvitationTxtVi;
+    @BindView(R.id.my_participation_txtVi)
+    TextView myParticipationTxtVi;
+    @BindView(R.id.my_insist_txtVi)
+    TextView myInsistTxtVi;
+    @BindView(R.id.my_news_txtVi)
+    TextView myNewsTxtVi;
+    @BindView(R.id.my_concern_txtVi)
+    TextView myConcernTxtVi;
+    @BindView(R.id.my_setting_txtVi)
+    TextView mySettingTxtVi;
+    @BindView(R.id.my_update_txtVi)
+    TextView myUpdateTxtVi;
+    @BindView(R.id.my_changepassword_txtVi)
+    TextView myChangepasswordTxtVi;
+
 
     private HomePresenter mhomePresenter;
     private LinearLayoutManager mManager;
@@ -67,15 +90,31 @@ public class HomeActivity extends BaseActivity implements HomeContact.View {
         //设置toolbar
         toolbar.setCenterText(getResources().getString(R.string.home_title));
         toolbar.setLeftIcon(getResources().getDrawable(R.drawable.user_img));
+        toolbar.setLeftViewOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHomeToast("头像");
+                mDrawerLayout.openDrawer(mSideBar);
+            }
+        });
         toolbar.setRightIcon(getResources().getDrawable(R.drawable.calenda_94dp));
+        toolbar.setRightViewOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, InsistActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //新建Presenter
         mhomePresenter = new HomePresenter(this);
 
+        //增加抽屉的活动范围
+        mhomePresenter.addDrawerRange(mDrawerLayout);
+
         //初始化SwipeRefreshLayout
         mhomePresenter.initSwipe(swipeLayout);
 
-        mDrawerLayout.setLinearLayout(mSideBar);
         //mhomePresenter.addLoadAnimation(mHomeAdapter);
 
         //设置RecyclerView
@@ -106,22 +145,29 @@ public class HomeActivity extends BaseActivity implements HomeContact.View {
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
+                if (!recyclerView.canScrollVertically(1)) {
+                    //showHomeToast("上拉加载");
+                    swipeLayout.setRefreshing(false);
+                    mhomePresenter.loadHomeData("zxw", 0, 0, lastId, 10);
+                }
                 //Log.i("HomeActivity2","("+dx+","+dy+")");
             }
 
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
-                if (newState == RecyclerView.SCROLL_STATE_IDLE && mManager.findLastVisibleItemPosition() + 1 == mHomeAdapter.getItemCount()) {
-                    swipeLayout.setRefreshing(false);
-                    //showHomeToast(lastId+"最后一个id");
-                    mhomePresenter.loadHomeData("zxw", 0, 0, lastId, 10);
-                    Log.i("HomeActivity1", "onScrollStateChanged()");
-
-                }
+//                if (newState ==RecyclerView.SCROLL_STATE_IDLE && mManager.findLastVisibleItemPosition() + 1 ==mHomeAdapter.getItemCount()) {
+//                    swipeLayout.setRefreshing(false);
+//                    //showHomeToast(lastId+"最后一个id");
+//                    mhomePresenter.loadHomeData("zxw",0,0,lastId,10);
+//                    Log.i("HomeActivity1","onScrollStateChanged()");
+//
+//                }
             }
         });
 
+
+        //内部点击事件
         mHomeAdapter.setOnRecyclerViewItemChildClickListener(new BaseQuickAdapter.OnRecyclerViewItemChildClickListener() {
             @Override
             public void onItemChildClick(BaseQuickAdapter baseQuickAdapter, View view, int i) {
@@ -133,6 +179,7 @@ public class HomeActivity extends BaseActivity implements HomeContact.View {
             }
         });
 
+        //第三方上拉加载
 //        mHomeAdapter.openLoadMore(true);
 //        mHomeAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
 //            @Override
@@ -158,6 +205,7 @@ public class HomeActivity extends BaseActivity implements HomeContact.View {
         return true;
     }
 
+    //首次加载，下拉刷新回调更改UI
     @Override
     public void loadHomeFirstData(List<InvitationBean> lists, int lastId) {
         //showHomeToast(lists.toString());
@@ -166,6 +214,7 @@ public class HomeActivity extends BaseActivity implements HomeContact.View {
         this.lastId = lastId;
     }
 
+    //上拉加载回调更改UI
     @Override
     public void loadHomeDownData(List<InvitationBean> lists, int lastId) {
 //        mHomeAdapter.addData(lists);
@@ -177,12 +226,14 @@ public class HomeActivity extends BaseActivity implements HomeContact.View {
         this.lastId = lastId;
     }
 
+    //打印Toast
     @Override
     public void showHomeToast(String toast) {
         Toast toast1 = Toast.makeText(this, toast, Toast.LENGTH_SHORT);
         toast1.show();
     }
 
+    //打开类别的详细界面
     @Override
     public void openHeadDetail(String string) {
         Intent intent = new Intent(this, InvitationActivity.class);
@@ -190,6 +241,7 @@ public class HomeActivity extends BaseActivity implements HomeContact.View {
         startActivity(intent);
     }
 
+    //打开下面邀约的详细界面
     @Override
     public void openOtherDetail(ArrayList<InvitationBean> mhomeBeanlists, int i) {
         Intent it = new Intent(this, InvitationDetailsActivity.class);
@@ -203,6 +255,14 @@ public class HomeActivity extends BaseActivity implements HomeContact.View {
         startActivityForResult(it, 1);
     }
 
+    //主界面参加或者取消活动更改UI
+    @Override
+    public void changeJoin() {
+        mHomeAdapter.setNewData(mhomePresenter.mhomeBeanlists);
+        mHomeAdapter.notifyDataSetChanged();
+    }
+
+    //接收下面邀约的回调
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -214,5 +274,52 @@ public class HomeActivity extends BaseActivity implements HomeContact.View {
         mHomeAdapter.notifyDataSetChanged();
     }
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.my_info_lLayout, R.id.my_invitation_txtVi, R.id.my_participation_txtVi, R.id.my_insist_txtVi, R.id.my_news_txtVi, R.id.my_concern_txtVi, R.id.my_setting_txtVi, R.id.my_update_txtVi, R.id.my_changepassword_txtVi} )
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.my_info_lLayout:
+                showHomeToast("我的个人信息");
+                break;
+            case R.id.my_changepassword_txtVi:
+                showHomeToast("修改密码");
+                break;
+            case R.id.my_invitation_txtVi:
+                intent = new Intent(this, InvitationActivity.class);
+                intent.putExtra("type", "我发起的");
+                startActivity(intent);
+                break;
+            case R.id.my_participation_txtVi:
+                intent = new Intent(this, InvitationActivity.class);
+                intent.putExtra("type", "我参与的");
+                startActivity(intent);
+                break;
+            case R.id.my_insist_txtVi:
+                intent = new Intent(HomeActivity.this, InsistActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.my_news_txtVi:
+                showHomeToast("我的信息");
+                break;
+            case R.id.my_concern_txtVi:
+                intent = new Intent(HomeActivity.this, MyConcernActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.my_setting_txtVi:
+                showHomeToast("设置");
+                break;
+            case R.id.my_update_txtVi:
+                showHomeToast("检查更新");
+                break;
+        }
+    }
 
 }

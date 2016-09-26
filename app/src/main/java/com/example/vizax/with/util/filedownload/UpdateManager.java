a017ce62d73ab000b4a0e587c0071ebac1ed91da
RuijiePan <281040711@qq.com>
2016-09-26 21:55:16 +0800

    更新模块完成

--- app/src/main/java/com/example/vizax/with/util/filedownload/UpdateManager.java
+++ app/src/main/java/com/example/vizax/with/util/filedownload/UpdateManager.java
@@ -2,11 +2,10 @@ package com.example.vizax.with.util.filedownload;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
 
-import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.vizax.with.util.DeviceUtils;
+import com.example.vizax.with.util.RxBus;
 
 /**
  * Created by prj on 2016/9/26.
@@ -40,7 +39,7 @@ public class UpdateManager {
                 .negativeText("暂不更新")
                 .onPositive((dialog, which) -> {
                     Intent intent = new Intent(mContext, DownLoadService.class);
-                    intent.putExtra("url","\"http://117.169.71.165/imtt.dd.qq.com/16891/580B22B6281E704CAB3079BC8210A88D.apk?mkey=57e9226712336c2a&f=8a5d&c=0&fsname=com.supertreasure_1.2_3.apk&csr=4d5s&p=.apk\n");
+                    intent.putExtra("url","http://117.169.71.165/imtt.dd.qq.com/16891/580B22B6281E704CAB3079BC8210A88D.apk?mkey=57e9226712336c2a&f=8a5d&c=0&fsname=com.supertreasure_1.2_3.apk&csr=4d5s&p=.apk\n");
                     mContext.startService(intent);
                 })
                 .build();
